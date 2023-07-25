e904808116fbb92214b5504388acdb7205da9bef
huthu198 <nguyenhuuthu1221@gmail.com>
2023-07-25 17:50:55 +0700

    no message

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Vector3 targetPosition;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleMouseDown();
+        }
+        MoveTowardsTarget();
+    }
+
+    private void MoveTowardsTarget()
+    {
+
+        if (transform.position != targetPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void HandleMouseDown()
+    {
+        targetPosition = GetMouseWorldPosition();
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = Camera.main.nearClipPlane; // Set độ sâu cho phép tính toán chuột trong không gian 3D
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
+
+
+
+   
+
+    
